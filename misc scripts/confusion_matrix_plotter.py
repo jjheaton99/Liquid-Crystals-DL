e3febf3cc667217a59324962976d54c51b80b7ff
Josh Heaton <jj_heaton@yahoo.co.uk>
2020-12-27 15:29:43 +0000

    report progress

--- misc scripts/confusion_matrix_plotter.py
+++ misc scripts/confusion_matrix_plotter.py
@@ -10,10 +10,38 @@ def display_confusion_matrix(y_true, y_pred, class_names, title='Confusion Matri
     con_mat_df = pd.DataFrame(con_mat_norm, index=class_names, columns=class_names)
     
     figsize=(len(class_names), len(class_names))
-    figure = plt.figure(figsize=figsize)
-    plt.suptitle(title)
-    sns.heatmap(con_mat_df, annot=True,cmap=plt.cm.Blues)
+    fig = plt.figure(figsize=figsize)
+    plt.suptitle(title, fontsize=16)
+    sns.set(font_scale=1.2)
+    sns.heatmap(con_mat_df, annot=True, cmap=plt.cm.Blues, cbar=False, square=True)
     plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+    plt.ylabel('True phase')
+    plt.xlabel('Predicted phase')
+    plt.show()
+    
+def display_2_confusion_matrices(y_true_1, y_pred_1, y_true_2, y_pred_2, class_names, 
+                                 title='Confusion Matrix', sub_title_1='', sub_title_2=''):
+    con_mat_1 = confusion_matrix(labels=y_true_1, predictions=y_pred_1).numpy()
+    con_mat_norm_1 = np.around(con_mat_1.astype('float') / con_mat_1.sum(axis=1)[:, np.newaxis], decimals=2)
+    con_mat_df_1 = pd.DataFrame(con_mat_norm_1, index=class_names, columns=class_names)
+    
+    con_mat_2 = confusion_matrix(labels=y_true_2, predictions=y_pred_2).numpy()
+    con_mat_norm_2 = np.around(con_mat_2.astype('float') / con_mat_2.sum(axis=1)[:, np.newaxis], decimals=2)
+    con_mat_df_2 = pd.DataFrame(con_mat_norm_2, index=class_names, columns=class_names)
+    
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
+    fig.suptitle(title, fontsize=16)
+    
+    sns.set(font_scale=1.2)
+    sns.heatmap(con_mat_df_1, annot=True, cmap=plt.cm.Blues, cbar=False, square=True, ax=ax1)
+    sns.heatmap(con_mat_df_2, annot=True, cmap=plt.cm.Blues, cbar=False, square=True, ax=ax2)
+    
+    ax1.set_title(sub_title_1)
+    ax1.set_ylabel('True phase')
+    ax1.set_xlabel('Predicted phase')
+    ax2.set_title(sub_title_2)
+    ax2.set_ylabel('True phase')
+    ax2.set_xlabel('Predicted phase')
+    
+    plt.tight_layout(w_pad=4.0, h_pad=1.5)
     plt.show()
--- models/Phase classifiers/con_mat_plotters.py
+++ models/Phase classifiers/con_mat_plotters.py
@@ -5,7 +5,7 @@ import sys
 sys.path.insert(1, 'C:/MPhys project/Liquid-Crystals-DL/misc scripts')
 
 from image_data_transformer import transform_image
-from confusion_matrix_plotter import display_confusion_matrix
+from confusion_matrix_plotter import display_confusion_matrix, display_2_confusion_matrices
 
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
@@ -50,39 +50,65 @@ def get_labels_and_preds(test_dir, model_name, sequential, image_size=256, evalu
 
     return y_true, y_pred    
 
+#sort true and predicted labels into correct phase order 
+#to display in the confusion matrix
+def rearrange_4_phase_labels(labels):
+    num_labels = np.shape(labels)[0]
+    new_labels = np.empty(num_labels)
+    
+    for index in range(num_labels):
+        if labels[index] == 0:
+            new_labels[index] = 2 
+        elif labels[index] == 1:
+            new_labels[index] = 0
+        elif labels[index] == 2:
+            new_labels[index] = 1
+        elif labels[index] == 3:
+            new_labels[index] = 3
+    
+    return new_labels
+
 def con_mat_4_phases(test_dir, model_name, title='Confusion Matrix', sequential=True, image_size=256, evaluate=True):
     y_true, y_pred = get_labels_and_preds(test_dir, model_name, sequential, image_size, evaluate)
     
-    #sort true and predicted labels into correct phase order 
-    #to display in the confusion matrix
-    def rearrange_labels(labels):
-        num_labels = np.shape(labels)[0]
-        new_labels = np.empty(num_labels)
-        
-        for index in range(num_labels):
-            if labels[index] == 0:
-                new_labels[index] = 2 
-            elif labels[index] == 1:
-                new_labels[index] = 0
-            elif labels[index] == 2:
-                new_labels[index] = 1
-            elif labels[index] == 3:
-                new_labels[index] = 3
-        
-        return new_labels
+    y_true = rearrange_4_phase_labels(y_true)
+    y_pred = rearrange_4_phase_labels(y_pred)
     
-    y_true = rearrange_labels(y_true)
-    y_pred = rearrange_labels(y_pred)
-    
-    class_names = ['isotropic',
-                   'nematic',
-                   'cholesteric', 
-                   'smectic']
+    class_names = ['Iso',
+                   'N',
+                   'N*', 
+                   'Sm']
     
     display_confusion_matrix(y_true, 
                              y_pred, 
                              class_names, 
                              title=title)
+    
+def con_mat_4_phases_2(test_dir, model_name_1, model_name_2, title='Confusion Matrix', sub_title_1='', sub_title_2='', 
+                       sequential_1=True, sequential_2=True, image_size_1=256, image_size_2=256, evaluate=True):
+    y_true_1, y_pred_1 = get_labels_and_preds(test_dir, model_name_1, sequential_1, image_size_1, evaluate)
+    
+    y_true_1 = rearrange_4_phase_labels(y_true_1)
+    y_pred_1 = rearrange_4_phase_labels(y_pred_1)
+    
+    y_true_2, y_pred_2 = get_labels_and_preds(test_dir, model_name_2, sequential_2, image_size_2, evaluate)
+    
+    y_true_2 = rearrange_4_phase_labels(y_true_2)
+    y_pred_2 = rearrange_4_phase_labels(y_pred_2)
+    
+    class_names = ['Iso',
+                   'N',
+                   'N*', 
+                   'Sm']
+    
+    display_2_confusion_matrices(y_true_1, 
+                                 y_pred_1,
+                                 y_true_2, 
+                                 y_pred_2,
+                                 class_names, 
+                                 title=title,
+                                 sub_title_1=sub_title_1,
+                                 sub_title_2=sub_title_2)
     
 def con_mat_smectic(test_dir, model_name, title, sequential=True, image_size=256, evaluate=True):
     y_true, y_pred = get_labels_and_preds(test_dir, model_name, sequential, image_size, evaluate)
@@ -95,10 +121,27 @@ def con_mat_smectic(test_dir, model_name, title, sequential=True, image_size=256
                              y_pred, 
                              class_names, 
                              title=title)
+    
+"""
+con_mat_4_phases('C:/MPhys project/Liquid-Crystals-DL/data/Prepared data/set2/test',
+                 'multi train 2nd run/all_128_6',
+                 title='Test set confusion matrix, 6 convolutional layers, all\n augmentations, 128 x 128 input size, 74.42% accuracy',
+                 image_size=128)
 
 con_mat_4_phases('C:/MPhys project/Liquid-Crystals-DL/data/Prepared data/set2/test',
                  'multi train 1st run/conv_2_flip_256',
-                 title='Test set confusion matrix, 2 convolutional layers, flip\n augmentations, 256 x 256 input size, 94.33% accuracy')
+                 title='Test set confusion matrix, 2 convolutional layers, flip\n augmentations, 256 x 256 input size, 94.33% accuracy',
+                 image_size=256)
+"""
+
+con_mat_4_phases_2('C:/MPhys project/Liquid-Crystals-DL/data/Prepared data/set2/test_no_iso',
+                   'multi train 1st run/conv_2_flip_256',
+                   'multi train 2nd run/all_128_6',
+                   'Test set confusion matrices for the most and least accurate models',
+                   '2 convolutional layers, flip\naugmentations, 256 x 256 input\nsize, 94.33% total accuracy',
+                   '6 convolutional layers, all\naugmentations, 128 x 128 input\nsize, 74.42% total accuracy',
+                   image_size_1=256,
+                   image_size_2=128)
 
 """    
 con_mat_smectic('C:/MPhys project/Liquid-Crystals-DL/data/Prepared data/smectic/test',
